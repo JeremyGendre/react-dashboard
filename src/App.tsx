4fe74806dc05eed4d16e9b3a500e100bec3cfbab
Jérémy Gendre <gendrejeremy@yahoo.fr>
2020-10-20 20:25:02 +0200

    adding the main components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './assets/App.css';
+import Header from "./component/Header";
+import Sidebar from "./component/Sidebar";
+import Content from "./component/Content";
 
 function App() {
   return (
     <div>
-      ouais
+        <Header/>
+        <div>
+            <Sidebar/>
+            <Content/>
+        </div>
     </div>
   );
 }
---
+++ src/component/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header(){
+    return (
+        <div className="sticky top-0 bg-gray-400">
+            ouais
+        </div>
+    );
+}
