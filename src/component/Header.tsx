274da824fa80f730f8a7a40a7c206bd54a5e1c37
Jérémy Gendre <gendrejeremy@yahoo.fr>
2020-10-20 22:46:08 +0200

    hiding the menu button when window screen >= md

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -6,7 +6,7 @@ import HeaderProfile from "./HeaderProfile";
 export default function Header(){
     return (
         <div className="sticky top-0 bg-gray-200 shadow-md w-full flex p-1">
-            <button className="mr-auto my-auto ml-4 w-10 h-10 rounded-full hover:text-gray-600 flex material-btn">
+            <button className="mr-auto my-auto ml-4 w-10 h-10 rounded-full hover:text-gray-600 md:hidden flex material-btn">
                 <AiOutlineMenu size={24} className="m-auto cursor-pointer"/>
             </button>
             <div className="flex ml-auto mr-4">
