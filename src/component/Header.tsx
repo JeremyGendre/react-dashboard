cca7170e382e7f69a3ffef4f4388051b5a3b7dab
Jérémy Gendre <gendrejeremy@yahoo.fr>
2020-10-20 22:43:33 +0200

    adding the menu icon and add an animation on button focus

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -6,13 +6,13 @@ import HeaderProfile from "./HeaderProfile";
 export default function Header(){
     return (
         <div className="sticky top-0 bg-gray-200 shadow-md w-full flex p-1">
-            <div className="mr-auto ml-4 flex md:hidden">
-                <AiOutlineMenu size={24} className="my-auto"/>
-            </div>
+            <button className="mr-auto my-auto ml-4 w-10 h-10 rounded-full hover:text-gray-600 flex material-btn">
+                <AiOutlineMenu size={24} className="m-auto cursor-pointer"/>
+            </button>
             <div className="flex ml-auto mr-4">
-                <div className="rounded-full border-2 w-10 h-10 flex cursor-pointer my-auto border-transparent mx-2 hover:text-gray-600 transition duration-150">
+                <button className="rounded-full border-2 w-10 h-10 flex cursor-pointer my-auto border-transparent mx-2 hover:text-gray-600 transition duration-150 material-btn">
                     <FaBell size={24} className="m-auto"/>
-                </div>
+                </button>
                 <HeaderProfile/>
             </div>
         </div>
--- src/component/HeaderProfile.tsx
+++ src/component/HeaderProfile.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function HeaderProfile() {
     return (
-        <div>
+        <button className="material-btn">
             <img className="rounded-full w-12 transition duration-150 cursor-pointer border-2 border-transparent hover:border-gray-400 hover:shadow-lg" title="profil" src={process.env.PUBLIC_URL + '/logo512.png'} alt="profile-avatar"/>
-        </div>
+        </button>
     );
 }
