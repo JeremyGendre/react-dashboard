5733a8beb02aeacd16e2f8c79b65854fec7ba249
Jérémy Gendre <jgendre@media-sante.com>
2020-10-30 16:59:22 +0100

    add arrow on top right of header profile submenu

--- src/component/header/HeaderProfile.tsx
+++ src/component/header/HeaderProfile.tsx
@@ -18,7 +18,7 @@ export default function HeaderProfile() {
             <button onClick={() => setShowMenu(prevState => !prevState)} className="material-btn m-auto">
                 <img className="rounded-full w-12 transition duration-150 cursor-pointer border-2 border-transparent hover:border-gray-400 hover:shadow-lg" title="profil" src={ process.env.PUBLIC_URL + '/logo512.png' } alt="profile-avatar"/>
             </button>
-            <div ref={menu} className={(showMenu ? 'flex flex-col' : 'hidden') + " absolute top-0 right-0 transform translate-y-14 bg-gray-600 text-white shadow-md rounded p-1"}>
+            <div ref={menu} className={(showMenu ? 'flex flex-col' : 'hidden') + " profile-submenu absolute top-0 right-0 transform translate-y-14 bg-gray-600 text-white shadow-md rounded p-1"}>
                 <HeaderProfileMenuLink label="Profile" url="/" icon={<FaUserAlt/>}/>
                 <HeaderProfileMenuLink label="Preferences" url="/" icon={<RiSettings4Fill/>}/>
                 <HeaderProfileMenuLink label="Log out" url="/" icon={<BiLogOut/>}/>
