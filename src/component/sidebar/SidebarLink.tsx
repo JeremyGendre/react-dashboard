27501b9352c2d8f062b81d01a1fcaccfbcf73911
Jérémy Gendre <jgendre@media-sante.com>
2021-06-07 16:07:29 +0200

    add active property in sidebar link

--- src/component/sidebar/SidebarLink.tsx
+++ src/component/sidebar/SidebarLink.tsx
@@ -1,15 +1,18 @@
 import React, {ReactNode} from "react";
-import { Link } from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
 
 type Props = {
     path: string,
     text: string,
+    active?: boolean,
     icon: ReactNode
 };
 
 export default function SidebarLink(props: Props) {
+    const location = useLocation();
+    const isActive = props.active ?? (location.pathname === props.path);
     return (
-        <Link to={props.path} className="material-large-btn overflow-hidden flex content-center w-full p-2 pr-4 pl-4 hover:bg-white transition duration-150 md:my-2 lg:my-0">
+        <Link to={props.path} className={"material-large-btn overflow-hidden flex content-center w-full p-2 pr-4 pl-4 hover:bg-white transition duration-150 md:my-2 lg:my-0 " + (isActive && ' bg-gray-100')}>
             <div className="my-auto mx-0 mr-6 md:mx-auto md:mr-auto lg:mr-6 lg:mx-0">
                 {props.icon}
             </div>
